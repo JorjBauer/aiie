d42a88e0835c1003ea0e7989e4189982bd65dcb9
Jorj Bauer <jorj@jorj.org>
2021-01-19 22:27:57 -0500

    remove some of the modules not in use

--- teensy/teensy-usb.cpp
+++ teensy/teensy-usb.cpp
@@ -2,12 +2,19 @@
 #include <USBHost_t36.h>
 #include "teensy-usb.h"
 
+// There are multiple hubs here because without them, USB ports won't work
+// if it's a chained hub device. From what I've read, most hubs are 4-port
+// devices -- but there are 7-port hubs, which use 2 chained hub chips, so
+// for all the USB ports to work on those hubs we have to have 2 hub objects
+// managing them. I've decided to limit this to 2 hub objects.
 USBHost myusb;
 USBHub hub1(myusb);
 USBHub hub2(myusb);
-USBHub hub3(myusb);
+
+// One could have multiple keyboards? I think I'm not going to support that
+// just yet, until I understand all of this better.
 KeyboardController keyboard1(myusb);
-KeyboardController keyboard2(myusb);
+//KeyboardController keyboard2(myusb);
 
 TeensyUSB::TeensyUSB()
 {
@@ -25,13 +32,13 @@ void TeensyUSB::init()
 void TeensyUSB::attachKeypress(keyboardCallback cb)
 {
   keyboard1.attachRawPress(cb);
-  keyboard2.attachRawPress(cb);
+  //  keyboard2.attachRawPress(cb);
 }
 
 void TeensyUSB::attachKeyrelease(keyboardCallback cb)
 {
   keyboard1.attachRawRelease(cb);
-  keyboard2.attachRawRelease(cb);
+  //  keyboard2.attachRawRelease(cb);
 }
 
 void TeensyUSB::maintain()
